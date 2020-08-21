96cee6e7f5413408acc24e243cddcbfc7d4ffcdd
Richard Tzanov <richard.tzanov@jdlt.co.uk>
2020-08-21 19:13:20 +0300

    add tracks total

--- client/src/PlaylistCard.tsx
+++ client/src/PlaylistCard.tsx
@@ -48,20 +48,23 @@ export const PlaylistCard: React.FC<PlaylistCardProps> = ({
 
   return (
     <Fragment>
-      <div className="border-2 w-1/4 m-4">
+      <div className="border-2 border-spotifyGreen w-1/3 m-4">
         <PlaylistImage images={playlist.images} />
 
-        <div className="relative">
-          <h2 className="mx-4 mt-2 italic text-xl font-bold w-8/12">
-            {playlist.name}
-          </h2>
-          <button
-            className="font-bold bg-spotifyGreen absolute top-0 right-0 mr-4 p-1 rounded clear-left"
-            onClick={() => handleSavePlaylist(playlist.id)}
-            disabled={savingPlaylist}
-          >
-            {savingPlaylist ? <LoadingSpinner /> : `save`}
-          </button>
+        <div className="divide-y divide-gray-400 mx-4">
+          <div className="relative">
+            <h2 className="mx-4 mt-2 mb-4 italic text-xl font-bold w-8/12 leading-tight">
+              {playlist.name}
+            </h2>
+            <button
+              className="font-bold bg-spotifyGreen absolute top-0 right-0 mr-4 p-1 rounded clear-left"
+              onClick={() => handleSavePlaylist(playlist.id)}
+              disabled={savingPlaylist}
+            >
+              {savingPlaylist ? <LoadingSpinner /> : `save`}
+            </button>
+          </div>
+          <h3 className="mx-4 pt-2">Tracks: {playlist.tracks.total}</h3>
         </div>
       </div>
     </Fragment>
